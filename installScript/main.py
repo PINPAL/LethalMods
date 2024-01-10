d23c28af873020faed63a1928d3f7aa2a33ffd15
PINPAL <18197217+PINPAL@users.noreply.github.com>
2024-01-10 08:06:18 +0000

    Overhaul Install Script with GUI

--- installScript/main.py
+++ installScript/main.py
@@ -1,47 +1,20 @@
+import eel
 import os
-import sys
 import winreg
 import vdf
-import ctypes
 import logging
 import zipfile
 import shutil
-import requests
-from tqdm import tqdm
+from urllib.request import urlopen
 
 # To build the exe run the following command in the terminal
-# python -m PyInstaller main.py -y --onefile -n Installer
+# python -m PyInstaller main.py -y --onefile -n LC-Mod-Installer --add-data "web;web" --hide-console hide-early --icon=web/favicon.ico
 
-logging.basicConfig(filename="errorlog.log",
-                    filemode='a',
-                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-                    datefmt='%H:%M:%S',
-                    level=logging.NOTSET)
-
-# function to print a nice header
-def printHeader(headerText):
-    print("\n======================================================================================")
-    print(headerText)
-    print("======================================================================================")
-
-# function to print an error message
-def printMessage(messageContent, isError):
-    ICON_ERROR = 0x30
-    ICON_INFO = 0x40
-    if (isError):
-        messageHeader = "Error!"
-        messageContent = messageContent + "\n\n Tell Josh the script is broken!"
-        icon = ICON_ERROR
-    else:
-        messageHeader = "Success!"
-        icon = ICON_INFO
-    ctypes.windll.user32.MessageBoxW(0, messageContent, messageHeader, 0x0 | icon)
-    # crash the program
-    if (isError):
-        sys.exit()
+# initialize eel
+eel.init('web', allowed_extensions=['.js', '.html'])
 
 def findInstallPath():
-    printHeader("Searching for Steam Install Path")
+    eel.addToConsole("Searching for Steam Install Path", False, True)
     # read the InstallPath key from the registry
     try:
         #connecting to key in registry
@@ -50,98 +23,151 @@ def findInstallPath():
         steamInstallPathArray = winreg.QueryValueEx(registryLocation,"InstallPath")
         steamInstallPath = steamInstallPathArray[0]
     except:
-        printMessage("Steam Install Path not found in registry", True)
+        eel.addToConsole("Steam Install Path not found in registry", True)
     # print the value
-    print("Steam is installed at: " + steamInstallPath + "\n")
+    eel.addToConsole("Steam is installed at: " + steamInstallPath + "\n")
+    eel.updateProgressText("findingSteamPath")
 
     lethalCompanySteamId = "1966720"
 
     # Read the libraryfolders.vdf file
-    printHeader("Searching for Lethal Company in Steam Libraries")
+    eel.addToConsole("Searching for Lethal Company in Steam Libraries", False, True)
     try:
         vdfFile = vdf.parse(open(steamInstallPath + r"\steamapps\libraryfolders.vdf"))
     except:
-        printMessage("Could not find libraryfolders.vdf", True)
+        eel.addToConsole("Could not find libraryfolders.vdf", True)
     # loop through the dictionary of dictionaries to libraries
     for libraryKey, libraryValues in vdfFile["libraryfolders"].items():
         # print the path value of the library
-        print("Searching for Lethal Company in: " + libraryValues["path"])
+        eel.addToConsole("Searching for Lethal Company in: " + libraryValues["path"])
         # loop through the apps dictionary and find the lethal company steam id
         for appKey, appValue in libraryValues["apps"].items():
             if appKey == lethalCompanySteamId:
                 lethalCompanyPath = libraryValues["path"] + r"\steamapps\common\Lethal Company"
-                print("--> Found Lethal Company in " + libraryValues["path"])
+                eel.addToConsole("--> Found Lethal Company in " + libraryValues["path"])
+                eel.updateProgressText("findingLethalCompany")
                 break
         else:
             continue
 
-    printHeader("Checking Installation is Valid")
-    print("Checking in: " + lethalCompanyPath)
+    eel.addToConsole("Checking Installation is Valid", False, True)
+    eel.addToConsole("Checking in: " + lethalCompanyPath)
     # Check if Lethal Company.exe exists
     if not (os.path.isfile(lethalCompanyPath + r"\Lethal Company.exe")):
-        printMessage("Lethal Company.exe not found in \n" + lethalCompanyPath, True) 
+        eel.addToConsole("Lethal Company.exe not found in \n" + lethalCompanyPath, True)
     else:
+        eel.addToConsole("Lethal Company.exe found in \n" + lethalCompanyPath)
+        eel.updateProgressText("checkingInstallation")
         return lethalCompanyPath
 
-def download(url: str, fname: str, chunk_size=1024):
-    resp = requests.get(url, stream=True)
-    total = int(resp.headers.get('content-length', 0))
-    with open(fname, 'wb') as file, tqdm(
-        desc=fname,
-        total=total,
-        unit='iB',
-        unit_scale=True,
-        unit_divisor=1024,
-    ) as bar:
-        for data in resp.iter_content(chunk_size=chunk_size):
-            size = file.write(data)
-            bar.update(size)
+def download(url: str, filename: str):
+    readBytes = 0
+    chunkSize = 1024
+    # Open the URL address.
+    with urlopen(url) as r:
+        # Tell the window the amount of bytes to be downloaded.
+        totalSize = 0
+        previousSize = 0
+        try:
+            totalSize = int(r.info()["Content-Length"])
+        except:
+            eel.addToConsole("Could not get Content-Length", True)
+        eel.addToConsole("Total: " + str(round(totalSize / 1024 / 1024, 2)) + "MB")
+        with open(filename, "ab") as f:
+            while True:
+                # Read a piece of the file we are downloading.
+                chunk = r.read(chunkSize)
+                # If the result is `None`, that means data is not
+                # downloaded yet. Just keep waiting.
+                if chunk is None:
+                    continue
+                # If the result is an empty `bytes` instance, then
+                # the file is complete.
+                elif chunk == b"":
+                    break
+                # Write into the local file the downloaded chunk.
+                f.write(chunk)
+                readBytes += chunkSize
+                # Check if the last update was more than 1MB ago.
+                if (previousSize + 1048576) < readBytes :
+                    # Tell the window how many bytes we have received.
+                    eel.updateDownloadProgress(readBytes, totalSize)
+                    previousSize = readBytes
 
+# ==============================
 # main program
 # ==============================
-try:
-    lethalCompanyPath = findInstallPath()
-    print("Lethal Company is installed at: " + lethalCompanyPath)
-
-    printHeader("Downloading the latest version of LethalMods")
-    pathToZipFile = lethalCompanyPath + "\LethalMods.zip"
-    # urllib.request.urlretrieve("https://codeload.github.com/PINPAL/LethalMods/zip/refs/heads/main", pathToZipFile)
-    download("https://codeload.github.com/PINPAL/LethalMods/zip/refs/heads/main", pathToZipFile)
-
-    printHeader("Extracting LethalMods")
-    # extract the zip file
-    with zipfile.ZipFile(pathToZipFile, 'r') as zip_ref:
-        zip_ref.extractall(lethalCompanyPath + r"\LethalMods")
-
-    printHeader("Deleting old LethalMods")
-    # delete BepInEx folder
+@eel.expose
+def pythonMain():
+    eel.addToConsole("Running Python.." ) 
     try:
-        shutil.rmtree(lethalCompanyPath + r"\BepInEx")
-        os.remove(lethalCompanyPath + r"\doorstop_config.ini")
-        os.remove(lethalCompanyPath + r"\winhttp.dll")
-    except:
-        print("BepInEx,doorstop_config or winhttp not found")
-    
-    printHeader("Moving new LethalMods")
-    try:
-        # move the new BepInEx folder
-        shutil.move(lethalCompanyPath + r"\LethalMods\LethalMods-main\LethalCompany\BepInEx", lethalCompanyPath)
-        # move the new doorstop_config.ini
-        shutil.move(lethalCompanyPath + r"\LethalMods\LethalMods-main\LethalCompany\doorstop_config.ini", lethalCompanyPath)
-        # move the new winhttp.dll
-        shutil.move(lethalCompanyPath + r"\LethalMods\LethalMods-main\LethalCompany\winhttp.dll", lethalCompanyPath)
-    except:
-        printMessage("Failed to move files", True)
+        lethalCompanyPath = findInstallPath()
 
-    printHeader("Cleaning up LethalMods")
-    try:
-        shutil.rmtree(lethalCompanyPath + r"/LethalMods")
-        os.remove(lethalCompanyPath + r"/LethalMods.zip")
-    except:
-        printMessage("Failed to delete LethalMods", True)
+        eel.updateProgressText("downloadingMods", True)
+        eel.addToConsole("Downloading the latest version of Lethal Company Mods", False, True)
+        eel.addToConsole("Please wait, this may take a while..")
+        pathToZipFile = lethalCompanyPath + r"\LethalMods.zip"
+        download("https://codeload.github.com/PINPAL/LethalMods/zip/refs/heads/main", pathToZipFile)
+        eel.addToConsole("LethalMods downloaded to: " + pathToZipFile)
 
-    # finished
-    printMessage("LethalMods installed successfully", False)
-    sys.exit()
-except:
-    logging.exception("Something awful happened!")
+        eel.addToConsole("Extracting LethalMods", False, True)
+        eel.updateProgressText("downloadingMods")
+        # extract the zip file
+        with zipfile.ZipFile(pathToZipFile, 'r') as zip_ref:
+            zip_ref.extractall(lethalCompanyPath + r"\LethalMods")
+
+        eel.updateProgressText("extractingMods")
+        eel.addToConsole("Deleting old LethalMods", False, True)
+        # delete BepInEx folder
+        try:
+            shutil.rmtree(lethalCompanyPath + r"\BepInEx")
+            eel.addToConsole("BepInEx deleted")
+        except:
+            eel.addToConsole("BepInEx Folder not found", True)
+        # delete doorstop_config.ini
+        try:
+            os.remove(lethalCompanyPath + r"\doorstop_config.ini")
+            eel.addToConsole("doorstop_config.ini deleted")
+        except:
+            eel.addToConsole("doorstop_config.ini not found", True)
+        # delete winhttp.dll
+        try:
+            os.remove(lethalCompanyPath + r"\winhttp.dll")
+            eel.addToConsole("winhttp.dll deleted")
+        except:
+            eel.addToConsole("winhttp.dll not found", True) 
+
+        eel.updateProgressText("removingOldMods")
+        eel.addToConsole("Moving Mod Files", False, True)
+        try:
+            # move the new BepInEx folder
+            shutil.move(lethalCompanyPath + r"\LethalMods\LethalMods-main\LethalCompany\BepInEx", lethalCompanyPath)
+            eel.addToConsole("BepInEx moved")
+            # move the new doorstop_config.ini
+            shutil.move(lethalCompanyPath + r"\LethalMods\LethalMods-main\LethalCompany\doorstop_config.ini", lethalCompanyPath)
+            eel.addToConsole("doorstop_config.ini moved")
+            # move the new winhttp.dll
+            shutil.move(lethalCompanyPath + r"\LethalMods\LethalMods-main\LethalCompany\winhttp.dll", lethalCompanyPath)
+            eel.addToConsole("winhttp.dll moved")
+        except:
+            eel.addToConsole("Failed to move files", True)
+
+        eel.updateProgressText("movingNewMods")
+        eel.addToConsole("Cleaning up LethalMods", False, True)
+        try:
+            shutil.rmtree(lethalCompanyPath + r"/LethalMods")
+            eel.addToConsole("LethalMods deleted")
+            os.remove(lethalCompanyPath + r"/LethalMods.zip")
+            eel.addToConsole("LethalMods.zip deleted")
+        except:
+            eel.addToConsole("Failed to delete LethalMods", True)
+
+        # finished
+        eel.updateProgressText("cleaningUp")
+        eel.updateProgressText("complete")
+        eel.addToConsole("LethalMods installed successfully", False, True, True)
+    except:
+        logging.exception("Something awful happened!")
+
+# run the GUI
+eel.start('main.html', size=(1000, 540), Block=False ) 
---
+++ installScript/web/js/main.js
@@ -0,0 +1,89 @@
+const consoleParent = document.getElementById("console");
+const console = document.getElementById("console").getElementsByTagName("div")[0];
+const progressSteps = document.getElementsByClassName("progressCheck");
+
+const completeSound = new Audio("assets/complete.mp3");
+completeSound.volume = 0.2;
+const errorSound = new Audio("assets/error.mp3");
+errorSound.volume = 0.2;
+
+var totalProgress = 0;
+
+function start() {
+	const startButton = document.getElementById("startButton");
+	startButton.style.display = "none";
+
+	const mainProgressBar = document.getElementById("mainProgressBar");
+	mainProgressBar.style.opacity = 1;
+	addToConsole("Starting...", false, true);
+	// start the python script
+	try {
+		eel.pythonMain();
+	} catch (error) {
+		addToConsole("Error: Python script failed to start.", true);
+		addToConsole(error, true);
+	}
+}
+
+eel.expose(addToConsole); // Expose this function to Python
+function addToConsole(text, isError = false, isHeader = false, isSuccess = false) {
+	// create elements
+	let span = document.createElement("span");
+	span.innerHTML = text;
+	// add classes if needed
+	if (isHeader) {
+		span.classList.add("header");
+	}
+	if (isError) {
+		span.classList.add("error");
+		errorSound.play();
+	}
+	if (isSuccess) {
+		span.classList.add("success");
+		completeSound.play();
+	}
+	// add to console
+	console.append(span);
+	// scroll to bottom
+	console.scrollTo(0, console.scrollHeight);
+}
+
+function updateProgressBar(elementId, progress) {
+	let progressBarInner = document.getElementById(elementId).getElementsByClassName("progressBarInner")[0];
+	progressBarInner.style.maxWidth = progress + "%";
+}
+eel.expose(updateDownloadProgress); // Expose this function to Python
+function updateDownloadProgress(current, total) {
+	if (total > 0) {
+		progress = Math.round((current / total) * 100);
+		currentMB = Math.round(current / 1024 / 1024);
+		totalMB = Math.round(total / 1024 / 1024);
+		addToConsole(`Downloading Mods: ${currentMB < 10 ? "0" : ""}${currentMB}/${totalMB} MB     |     ${progress}%`);
+		progress = totalProgress + (progress / 100) * (100 / 9);
+		updateProgressBar("mainProgressBar", progress);
+	} else {
+		addToConsole("Downloading Mods: " + currentMB + "MB");
+	}
+}
+
+eel.expose(updateProgressText); // Expose this function to Python
+function updateProgressText(elementId, inProgress = false) {
+	let element = document.getElementById(elementId);
+	if (inProgress) {
+		element.classList.add("inProgress");
+	} else {
+		element.classList.add("checked");
+		element.classList.remove("inProgress");
+	}
+
+	//  loop though each element in progressSteps and check if it has "checked" class
+	let progress = 0;
+	for (let i = 0; i < progressSteps.length; i++) {
+		let element = progressSteps[i];
+		if (element.classList.contains("checked")) {
+			progress += 1;
+		}
+	}
+	totalProgress = (progress / progressSteps.length) * 100;
+	updateProgressBar("mainProgressBar", totalProgress);
+}
